ec9a0a7225af734bca1304ba703af73d30f6fcfd
Annabelle Liu <lucky620932@gmail.com>
2022-07-20 13:37:18 -0700

    Home Page & Senior Quotes

---
+++ WordsGoingApp/SeniorQuotesViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SeniorQuotesViewController.swift
+//  WordsGoingApp
+//
+//  Created by Scholar on 7/20/22.
+//
+
+import UIKit
+
+class SeniorQuotesViewController: UIViewController {
+
+    @IBOutlet weak var lavenderImage2: UIImageView!
+    @IBOutlet weak var lavenderImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lavenderImage.alpha = 0.4
+        lavenderImage2.alpha = 0.4
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
